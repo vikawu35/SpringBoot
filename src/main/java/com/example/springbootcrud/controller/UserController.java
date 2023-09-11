2e206125f7ec0d3f3348268c0314e53c1f619cc8
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-09-11 11:37:21 +0900

    Update UserController.java

--- src/main/java/com/example/springbootcrud/controller/UserController.java
+++ src/main/java/com/example/springbootcrud/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.springbootcrud.controller;
 
 import com.example.springbootcrud.model.User;
 import com.example.springbootcrud.service.UserServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-
+@RequestMapping("/users")
 public class UserController {
 
     private final UserServiceImpl userServiceImpl;
@@ -19,42 +20,40 @@ public class UserController {
         this.userServiceImpl = userServiceImpl;
     }
 
-    @GetMapping("/users")
+    @GetMapping()
     public String listUsers(Model model) {
         model.addAttribute("users", userServiceImpl.getAllUsers());
         return "index";
     }
 
-    @GetMapping("/users/new")
+    @GetMapping("/new")
     public String createUserForm(Model model) {
         model.addAttribute("user", new User());
         return "new";
     }
 
-    @PostMapping("/users")
-    public String createUser(@ModelAttribute User user) {
+    @PostMapping()
+    public String createUser(@Valid @ModelAttribute User user) {
         userServiceImpl.createUser(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users/{id}/edit")
+    @GetMapping("/{id}/edit")
     public String editUserForm(@PathVariable("id") Long id,
                            Model model) {
         userServiceImpl.getUserById(id).ifPresent(user -> model.addAttribute("user", user));
         return "edit";
     }
 
-    @PostMapping("/users/{id}")
-    public String updateUser(@PathVariable("id") Long id, @ModelAttribute User updatedUser) {
+    @PostMapping("/{id}")
+    public String updateUser(@Valid @PathVariable("id") Long id, @ModelAttribute User updatedUser) {
         userServiceImpl.updateUser(id, updatedUser);
         return "redirect:/users";
     }
 
-    @PostMapping("/users/{id}/delete")
+    @PostMapping("/{id}/delete")
     public String deleteUser(@PathVariable("id") Long id) {
         userServiceImpl.deleteUser(id);
         return "redirect:/users";
     }
-
-
 }
